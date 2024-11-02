7271fea3c294c2c570b0bda172243328327d7130
Andro Bakashvili <bakashviliandro@gmail.com>
2024-11-03 03:00:46 +0400

    update dashboard.lua

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -12,5 +12,51 @@ return {
 
         logo = string.rep("\n", 8) .. logo .. "\n\n"
         opts.config.header = vim.split(logo, "\n")
+        opts.config.center = {
+            {
+                action = "lua LazyVim.pick()()",
+                desc = " Find File",
+                icon = " ",
+                key = "f",
+            },
+            {
+                action = "ene | startinsert",
+                desc = " New File",
+                icon = " ",
+                key = "n",
+            },
+            {
+                action = 'lua LazyVim.pick("oldfiles")()',
+                desc = " Recent Files",
+                icon = " ",
+                key = "r",
+            },
+            {
+                action = 'lua LazyVim.pick("live_grep")()',
+                desc = " Find Text",
+                icon = " ",
+                key = "g",
+            },
+            {
+                action = "lua LazyVim.pick.config_files()()",
+                desc = " Config",
+                icon = " ",
+                key = "c",
+            },
+            {
+                action = 'lua require("persistence").load()',
+                desc = " Restore Session",
+                icon = " ",
+                key = "s",
+            },
+        }
+        opts.config.footer = function()
+            return { "" }
+        end
+
+        for _, button in ipairs(opts.config.center) do
+            button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+            button.key_format = "  %s"
+        end
     end,
 }
