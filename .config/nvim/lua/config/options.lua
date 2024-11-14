35ddcb610635dd10569cf3840fdecfdff814f9ce
Andro Bakashvili <bakashviliandro@gmail.com>
2024-11-14 17:26:39 +0400

    add custom options for editing html

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,12 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
 vim.opt.autoindent = true
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "html",
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+})
