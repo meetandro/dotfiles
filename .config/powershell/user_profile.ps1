66e1bd70725a921a7d6c7288710926cc399c3052
Andro Bakashvili <bakashviliandro@gmail.com>
2025-06-03 20:52:13 +0400

    update .gitignore, add utility list function

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,6 +2,7 @@
 sal vi nvim -Scope Global
 sal g git -Scope Global
 sal lg lazygit -Scope Global
+sal neofetch winfetch -Scope Global
 
 # Starship
 Invoke-Expression (&starship init powershell)
@@ -11,3 +12,8 @@ Import-Module -Name Terminal-Icons
 
 # PSReadLine
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+# List excluding dotfiles/folders
+function list {
+    Get-ChildItem | Where-Object { !($_.Name -like '.*') }
+}
